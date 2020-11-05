889689a7273dc328993de2a65987aa84d071659d
ANAS <anasoubida1@gmail.com>
2020-11-05 01:19:15 +0100

    ne pas prendre en considération

--- essai.R
+++ essai.R
@@ -1,6 +1,10 @@
 # Bonjour1, je m'appelle Toto !
 #j'ai 19 ans 
 #Bonne soirée à tous 
+<<<<<<< HEAD
 #ajout d'une ligne qui ne sert à rien  
 #hak zidk hadi achrif
 #wakwak a3ibad lah 
+=======
+#ajout d'une ligne qui ne sert à rien 
+>>>>>>> parent of 9497ca3... NEW LINE
