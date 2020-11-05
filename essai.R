d04abe63a0f655898a81fd7900fe8dad347ba9de
ANAS <anasoubida1@gmail.com>
2020-11-05 02:09:54 +0100

    final1

--- essai.R
+++ essai.R
@@ -4,10 +4,5 @@
 #ajout d'une ligne qui ne sert à rien  
 #hak zidk hadi achrif
 #wakwak a3ibad lah 
-<<<<<<< HEAD
 #ANA
-=======
-=======
-#ajout d'une ligne qui ne sert à rien 
-#>>>>>>> parent of 9497ca3... NEW LINE
->>>>>>> parent of 6440352... O
+
